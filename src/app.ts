fd21a7b5a13ac556883d89f2cf7e7c94ef46b9ec
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2023-10-24 21:27:59 -0300

    feat: created database

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
 export const app = fastify()
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Rafael Marques',
+    email: 'rafael@email.com',
+  },
+})
